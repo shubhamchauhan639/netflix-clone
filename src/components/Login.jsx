6af6a79fd01644e6b99f51200e833f1509299728
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-11-07 20:39:34 +0530

    sign up page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Header from './Header'
 
 const Login = () => {
+  const [isSignIn,setSignIn]= useState(true)
+  const toggleSignInForm =()=>{
+    setSignIn(!setSignIn)
+
+  }
   return (
     <div>
       <Header/>
@@ -9,7 +14,7 @@ const Login = () => {
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/a92a67ee-cd07-46a8-8354-c431a96a97b0/web/IN-en-20251103-TRIFECTA-perspective_8a65e995-9926-414c-83c5-f7cc9af10871_large.jpg" alt="background" />
              <form className="bg-black/80 p-12 rounded-md w-3/12 flex flex-col items-center gap-4 
                        absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-white text-3xl font-bold mb-4">Sign In</h1>
+        <h1 className="text-white text-3xl font-bold mb-4">{isSignIn ? "Sign In" : "Sign Up"}</h1>
 
         <input
           type="text"
@@ -26,6 +31,7 @@ const Login = () => {
         <button className="w-full bg-red-600 text-white font-semibold p-4 rounded-md hover:bg-red-700 transition">
           Sign In
         </button>
+       <label className='text-white mb-15 mr-57 text-2xl'><input type="checkbox" className='text-white w-5 h-5' /> Remember me</label> 
         <p className='p-4 text-white'>New to Netflix? Sign Up Now</p>
       </form>
       </div>
