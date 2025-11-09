2cb2fe516422bdba35ed1185e31ec1d835e6efaa
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-11-09 13:16:07 +0530

    validation in form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import Header from './Header'
-import { useState } from 'react'
+import { useState , useRef } from 'react'
+import { checkValidData } from '../utils/validate'
 
 const Login = () => {
+
   const [isSignIn,setSignIn]= useState(true)
+  const [errorMessage , setErrorMessage] = useState(null)
+  const email = useRef(null);
+  const password = useRef(null);
+  const handleButtonClick =()=>{
+    console.log(email.current.value);
+  console.log(password.current.value);
+const message = checkValidData(email.current.value , password.current.value)
+setErrorMessage(message)
+  }
   const toggleSignInForm =()=>{
     setSignIn(!setSignIn)
+  
+  
+  
 
   }
   return (
@@ -13,7 +27,7 @@ const Login = () => {
       <Header/>
       <div>
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/a92a67ee-cd07-46a8-8354-c431a96a97b0/web/IN-en-20251103-TRIFECTA-perspective_8a65e995-9926-414c-83c5-f7cc9af10871_large.jpg" alt="background" />
-             <form className="bg-black/80 p-12 rounded-md w-3/12 flex flex-col items-center gap-4 
+             <form onSubmit={(e)=>e.preventDefault()} className="bg-black/80 p-12 rounded-md w-3/12 flex flex-col items-center gap-4 
                        absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <h1 className="text-white text-3xl font-bold mb-4">{isSignIn ? "Sign In" : "Sign Up"}</h1>
 
@@ -24,18 +38,20 @@ const Login = () => {
         />}
 
         <input
+        ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 w-full bg-gray-700 text-white rounded-md outline-none"
         />
 
         <input
+        ref={password}
           type="password"
           placeholder="Password"
           className="p-4 w-full bg-gray-700 text-white rounded-md outline-none"
         />
-
-        <button className="w-full bg-red-600 text-white font-semibold p-4 rounded-md hover:bg-red-700 transition" onClick={toggleSignInForm}>
+<p className='text-red-600'>{errorMessage}</p>
+        <button className="w-full bg-red-600 text-white font-semibold p-4 rounded-md hover:bg-red-700 transition" onClick={handleButtonClick}>
           {isSignIn ? "Sign In" : "Sign Up"}
         </button>
        <label className='text-white mb-15 mr-57 text-2xl'><input type="checkbox" className='text-white w-5 h-5' /> Remember me</label> 
