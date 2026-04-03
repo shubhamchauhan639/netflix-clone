ae73aac0171c8bd60b713da7e17a337dd026797f
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-04-03 23:44:18 +0530

    add logout feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,74 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../utils/firebase";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Header = () => {
-  return (
-    <div className='absolute px-8 w-screen py-2 bg-gradient-to-b from-black'>
-      <img 
-      className = "w-50"
-      src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="Logo" />
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
 
-export default Header
+  // ✅ Auth listener
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+
+        dispatch(
+          addUser({
+            uid,
+            email,
+            displayName,
+            photoURL,
+          })
+        );
+
+        navigate("/brouse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch, navigate]);
+
+  // ✅ Sign out
+  const handleSignOut = () => {
+    signOut(auth).catch(() => navigate("/error"));
+  };
+
+  return (
+    <div className="absolute px-8 w-screen py-2 bg-gradient-to-b from-black flex justify-between items-center">
+      
+      {/* Logo */}
+      <img
+        className="w-44"
+        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo"
+      />
+
+      {/* User section */}
+      {user && (
+        <div className="flex items-center gap-4">
+          <img
+            className="w-10 h-10 rounded-full hidden md:block"
+            src={user?.photoURL}
+            alt="user"
+          />
+
+          <button
+            onClick={handleSignOut}
+            className="text-white font-semibold"
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
