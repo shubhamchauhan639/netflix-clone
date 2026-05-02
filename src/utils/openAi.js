c2f515b786e008ed2834b2f6a2944a6aa40c3edc
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-05-02 22:04:52 +0530

    open ai key add

---
+++ src/utils/openAi.js
@@ -0,0 +1,7 @@
+// const OpenAI = require('openai-api');
+
+// // Load your key from an environment variable or secret management service
+// // (do not include your key directly in your code)
+// const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+// const openai = new OpenAI(OPENAI_API_KEY);
