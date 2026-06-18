92d85ccfae7a9af176332c17ecb72d8040cf3638
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-06-18 23:59:31 +0530

    fix structure

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -3,19 +3,18 @@ import { useSelector } from 'react-redux';
 import VideoBackground from './VideoBackground';
 import VideoTitle from './VideoTitle';
 
-
 const MainContainer = () => {
-  const movies = useSelector(store=>store.movies?.nowPlayingMovies)
+  const movies = useSelector(store => store.movies?.nowPlayingMovies)
 
   if (movies === null) return;
-  const mainMovie = movies[0]||movies[1]||movies[2]||movies[3]||movies[4]||movies[5]||movies[6]||movies[7]||movies[8]  ;
+  const mainMovie = movies[0] || movies[1] || movies[2] || movies[3] || movies[4] || movies[5] || movies[6] || movies[7] || movies[8];
 
 
-  const {original_title , overview , id} = mainMovie;
+  const { original_title, overview, id } = mainMovie;
   return (
-  <div className="pt-20 relative">
-            <VideoBackground movieId = {id} />
-         <VideoTitle title = {original_title} overview = {overview} />
+    <div className="pt-20 relative">
+      <VideoBackground movieId={id} />
+      <VideoTitle title={original_title} overview={overview} />
 
     </div>
   )
